c9eb83b18eb4d0b0e72d24753655cab355943222
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-07-03 00:22:22 +0100

    layout and temp files

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui/container";
+import { Text } from "tamagui";
+
+export default function HomeTab() {
+    return (
+        <Container>
+            <Text>Home Tab</Text>
+        </Container>
+    )
+}
