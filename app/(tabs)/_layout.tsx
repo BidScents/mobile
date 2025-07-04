b95dac899991ec7f48c9ff7e16d8021c54a77fa6
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-07-04 23:05:13 +0100

    correcting order

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,8 +5,8 @@ export default function TabsLayout() {
     return (
         <Tabs>
             <Tabs.Screen name="index" />
-            <Tabs.Screen name="create-listing" />
             <Tabs.Screen name="search" />
+            <Tabs.Screen name="create-listing" />
             <Tabs.Screen name="inbox" />
             <Tabs.Screen name="profile" />
         </Tabs>
