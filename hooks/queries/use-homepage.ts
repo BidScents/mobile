c64663f4b27e76b35770108a52ea65fe45c8ee73
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-07-09 13:40:21 +0100

    useHomepage hook

---
+++ hooks/queries/use-homepage.ts
@@ -0,0 +1,13 @@
+import { HomepageService } from "@bid-scents/shared-sdk"
+import { useQuery } from "@tanstack/react-query"
+import { queryKeys } from "./query-keys"
+
+export const useHomepage = () => {
+    return useQuery({
+        queryKey: queryKeys.homepage,
+        queryFn: () => HomepageService.getHomepageV1HomepageGet(),
+        staleTime: 5 * 60 * 1000,
+        gcTime: 10 * 60 * 1000,
+        retry: 2,
+    })
+}
