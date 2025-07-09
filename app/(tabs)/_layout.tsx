eb12b621d39fa5339e61494d71a5e59f31b34aea
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-07-09 05:58:54 +0100

    tabbar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,114 @@
+import { Ionicons } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
+import * as Haptics from 'expo-haptics';
 import { Tabs } from "expo-router";
-
+import { StyleSheet } from 'react-native';
+import { useTheme } from 'tamagui';
 
 export default function TabsLayout() {
-    return (
-        <Tabs>
-            <Tabs.Screen name="index" />
-            <Tabs.Screen name="search" />
-            <Tabs.Screen name="create-listing" />
-            <Tabs.Screen name="inbox" />
-            <Tabs.Screen name="profile" />
-        </Tabs>
-    )
+  const theme = useTheme();
+
+  const handleHapticFeedback = () => {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+  };
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: true,
+        tabBarActiveTintColor: theme.foreground?.val,
+        tabBarInactiveTintColor: theme.mutedForeground?.val,
+        tabBarLabelStyle: {
+          fontSize: 12,
+          paddingTop: 3,
+          fontWeight: '500',
+        },
+        tabBarStyle: {
+          position: 'absolute',
+        },
+        tabBarBackground: () => (
+          <BlurView  intensity={80} style={{ 
+            ...StyleSheet.absoluteFillObject,
+            overflow: 'hidden',
+            backgroundColor: 'transparent',
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+           }} />
+        ),
+      }}
+      screenListeners={{
+        tabPress: () => handleHapticFeedback(),
+      }}
+    >
+      <Tabs.Screen 
+        name="index" 
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "home" : "home-outline"} 
+              size={24} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      
+      <Tabs.Screen 
+        name="search" 
+        options={{
+          title: "Search",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "search" : "search-outline"} 
+              size={24} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      
+      <Tabs.Screen 
+        name="create-listing" 
+        options={{
+          title: "Add",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "add-circle" : "add-circle-outline"} 
+              size={28} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      
+      <Tabs.Screen 
+        name="inbox" 
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "chatbubbles" : "chatbubbles-outline"} 
+              size={24} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+      
+      <Tabs.Screen 
+        name="profile" 
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused, color }) => (
+            <Ionicons 
+              name={focused ? "person" : "person-outline"} 
+              size={24} 
+              color={color} 
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
