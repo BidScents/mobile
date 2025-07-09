519b029da2f80bc7550fae8d9814b53f29b2ab1e
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-07-09 14:14:29 +0100

    temporary detailed listings page

---
+++ app/listing/[id].tsx
@@ -0,0 +1,10 @@
+import { Text, YStack } from "tamagui";
+
+export default function ListingScreen() {
+    
+    return (
+        <YStack>
+            <Text>Listing Screen</Text>
+        </YStack>
+    )
+}
