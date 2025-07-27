b6ab7c3e9990d44ff112b18b14f6972ebff8b492
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-07-27 04:04:04 +0100

    profile dynamic route

---
+++ app/profile/[id].tsx
@@ -0,0 +1,10 @@
+import { Container } from "@/components/ui/container";
+import { Text } from "tamagui";
+
+export default function ProfileScreen() {
+  return (
+    <Container variant="padded" safeArea={["top"]} backgroundColor="$background">
+      <Text>Profile Screen</Text>
+    </Container>
+  );
+}
