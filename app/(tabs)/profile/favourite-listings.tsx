42f813430a32b58cd0fabd60e5191ce8e063c75a
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-08-01 03:08:29 +0100

    temp favorite listings page

--- app/(tabs)/profile/favourite-listings.tsx
+++ app/(tabs)/profile/favourite-listings.tsx
@@ -1,10 +1,20 @@
+import { ListingCard } from "@/components/listing/listing-card";
 import { Container } from "@/components/ui/container";
-import { Text } from "tamagui";
+import { useUserFavorites } from "@/hooks/queries/use-listing";
+import { FlashList } from "@shopify/flash-list";
 
 export default function FavouriteListingsScreen() {
+  const { data: listings, isLoading, error } = useUserFavorites();
   return (
-    <Container variant="padded" safeArea={["top"]} backgroundColor="$background">
-      <Text>Favourite Listings Screen</Text>
+    <Container variant="padded" safeArea={false} backgroundColor="$background">
+      <FlashList
+        data={listings || []}
+        renderItem={({ item }) => <ListingCard listing={item} />}
+        keyExtractor={(item) => item.id}
+        estimatedItemSize={200}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+      />
     </Container>
   );
 }
