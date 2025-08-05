61ef943abfd4cf3f61fbb176a6c932c0b94d1dd2
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-08-05 04:59:53 +0100

    formating details section

--- components/listing/listing-details-section.tsx
+++ components/listing/listing-details-section.tsx
@@ -1,50 +1,65 @@
 import { ListingDetails } from "@bid-scents/shared-sdk";
 import { Text, XStack, YStack } from "tamagui";
+import { formatAllCapsText } from "../../utils/utility-functions";
 
-
-export function ListingDetailsSection({ listing }: { listing: ListingDetails }) {
-
-      const listingDetails = [
-        {
-          "title": "Category",
-          "value": listing?.category,
-        },
-        {
-          "title": "Purchase Year",
-          "value": listing?.purchase_year, 
-        },
-        {
-          "title": "Box Condition",
-          "value": listing?.box_condition,
-        },
-        {
-          "title": "Quantity",
-          "value": listing?.quantity,
-        },
-        {
-          "title": "Volume",
-          "value": listing?.volume,
-        },
-        {
-          "title": "Remaining Percentage",
-          "value": listing?.remaining_percentage,
-        },
-        {
-          "title": "Batch Code",
-          "value": listing?.batch_code ? listing?.batch_code : "N/A",
-        },
-      ]
-    return (
-        <YStack gap="$2">
-            <Text fontSize="$7" fontWeight="500">Details</Text>
-            <YStack gap="$1" bg="$muted" borderRadius="$6" px="$4" py="$2">
-                {listingDetails.map((detail) => (
-                <XStack key={detail.title} alignItems="center" justifyContent="space-between" gap="$2" py="$3">
-                    <Text fontSize="$4" fontWeight="500">{detail.title}</Text>
-                    <Text fontSize="$4" fontWeight="400">{detail.value}</Text>
-                </XStack>
-            ))}
-            </YStack>
-        </YStack>
-    )
+export function ListingDetailsSection({
+  listing,
+}: {
+  listing: ListingDetails;
+}) {
+  const listingDetails = [
+    {
+      title: "Category",
+      value: formatAllCapsText(listing?.category || ""),
+    },
+    {
+      title: "Purchase Year",
+      value: listing?.purchase_year,
+    },
+    {
+      title: "Box Condition",
+      value: formatAllCapsText(listing?.box_condition || ""),
+    },
+    {
+      title: "Quantity",
+      value: listing?.quantity,
+    },
+    {
+      title: "Volume",
+      value: listing?.volume,
+    },
+    {
+      title: "Remaining Percentage",
+      value: listing?.remaining_percentage,
+    },
+    {
+      title: "Batch Code",
+      value: listing?.batch_code ? listing?.batch_code : "N/A",
+    },
+  ];
+  return (
+    <YStack gap="$3">
+      <Text fontSize="$7" fontWeight="500">
+        Listing Details
+      </Text>
+      <YStack gap="$1" bg="$muted" borderRadius="$6" px="$4" py="$2">
+        {listingDetails.map((detail) => (
+          <XStack
+            key={detail.title}
+            alignItems="center"
+            justifyContent="space-between"
+            gap="$2"
+            py="$2.5"
+          >
+            <Text fontSize="$5" fontWeight="500">
+              {detail.title}
+            </Text>
+            <Text fontSize="$5" fontWeight="400">
+              {detail.value}
+            </Text>
+          </XStack>
+        ))}
+      </YStack>
+    </YStack>
+  );
 }
