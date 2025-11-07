9238db7e7dc25ab50ff0bd20ece2b9a141c7ddf8
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-11-07 19:59:21 +0000

    redundant pages

--- app/(tabs)/profile/_layout.tsx
+++ app/(tabs)/profile/_layout.tsx
@@ -21,14 +21,11 @@ export default function ProfileLayout() {
     >
       <Stack.Screen name="index" options={{ title: "Profile", headerLeft: () => null }} />
       <Stack.Screen name="[id]" options={{ title: "Profile", headerShown: false }} />
-      <Stack.Screen name="badges" options={{ title: "Badges" }} />
       <Stack.Screen name="favourite-listings" options={{ title: "Favourite Listings" }} />
       <Stack.Screen name="orders" options={{ title: "Orders" }} />
       <Stack.Screen name="seller-dashboard" options={{ title: "Seller Dashboard" }} />
       <Stack.Screen name="settings" options={{ title: "Settings", headerShown: false }} />
       <Stack.Screen name="help" options={{ title: "Help" }} />
-      <Stack.Screen name="about-us" options={{ title: "About Us" }} />
-      <Stack.Screen name="legal" options={{ title: "Legal" }} />
       <Stack.Screen name="edit-profile" options={{ title: "Edit Profile", headerLargeTitle: true }} />
       <Stack.Screen name="report-profile" options={{ title: "Report Profile", headerLargeTitle: true }} />
     </Stack>
